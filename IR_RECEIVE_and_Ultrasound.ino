c5c3cd4383a9b8a3465badb58914f778abf1b8d2
sufutree <95114438+sufutree@users.noreply.github.com>
2022-01-04 11:14:31 +0800

    Update IR_RECEIVE_and_Ultrasound.ino

--- IR_RECEIVE_and_Ultrasound.ino
+++ IR_RECEIVE_and_Ultrasound.ino
@@ -9,14 +9,17 @@ void setup() {
   Serial.begin (9600);             // Serial Port begin
   pinMode(trigPin, OUTPUT);        // 定義輸入及輸出
   pinMode(echoPin, INPUT);
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);//紅外線遠距設定
   Serial.println("CLEARDATA");     //清空數據
   Serial.println("LABEL,Date,Time,mills,distance(cm)");
 }
 void loop(){ 
-  if (IrReceiver.decode()) {
-    if (IrReceiver.decodedIRData.address == 0x0) {
-      if (IrReceiver.decodedIRData.command == 0x16) {
+  if (IrReceiver.decode()) //紅外線接收到
+  {
+    if (IrReceiver.decodedIRData.address == 0x0) //紅外線接收指定數值
+    {
+      if (IrReceiver.decodedIRData.command == 0x16)//紅外線接收指定數值
+      {
         digitalWrite(trigPin, LOW);
         delayMicroseconds(5);
         digitalWrite(trigPin, HIGH);     // 給 Trig 高電位，持續 10微秒
@@ -29,7 +32,7 @@ void loop(){
         runTime = millis();
         Serial.println( (String) "DATA,DATE,TIME," + runTime + "," + cm );
       }
-      IrReceiver.resume();
+      IrReceiver.resume();//紅外線重新接收
     }
     delay(10);
   }
